0cf950412bf406c1e9495449d04bcd920ed64c5b
Özgür ÖZALP <ozgur@altogic.com>
2022-11-11 10:46:40 +0300

    deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginWithMagicLinkComponent } from './pages/login-with-magic-link/login-with-magic-link.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthRedirectComponent } from './pages/auth-redirect/auth-redirect.component';
+import { AvatarComponent } from './components/avatar/avatar.component';
+import { SessionsComponent } from './components/sessions/sessions.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		LoginComponent,
+		RegisterComponent,
+		HomeComponent,
+		LoginWithMagicLinkComponent,
+		ProfileComponent,
+  AuthRedirectComponent,
+  AvatarComponent,
+  SessionsComponent,
+  UserInfoComponent,
+	],
+	imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
